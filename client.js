7fe33a41b97870eb803650e7a6544bdf190d97b5
Tangent314 <blairmclaughlin89@gmail.com>
2019-10-04 16:51:56 -0500

    create addEmployee function

--- client.js
+++ client.js
@@ -1,8 +1,26 @@
-
+let totalSalary = 0;
 
 $(document).ready(onReady);
 
 function onReady() {
     $('#submitButton').on('click', addEmployee);
     $('tbody').on('click','.deleteButton', deleteEmployee)
+}
+
+function addEmployee() {
+    const firstName = $('#firstName').val();
+    const lastName = $('#lastName').val();
+    const id = $('#id').val();
+    const title = $('#title').val();
+    const annualSalary = $('#annualSalary').val();
+    let employee = `<tr><td>${firstName}</td>
+                        <td>${lastName}</td>
+                        <td>${id}</td>
+                        <td>${title}</td>
+                        <td>${annualSalary}</td>
+                        <td><button type="button" class="deleteButton">Delete</button></td></tr>`;
+    $('tbody').append(employee);
+    totalSalary += Number(annualSalary);
+    let monthlyCost = totalSalary/12;
+    $('#cash').text(monthlyCost.toFixed(2));
 }
